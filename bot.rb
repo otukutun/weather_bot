3c45d753db8d040f3a0ae5d680e4ee72fed23b83
otukutun <orehaorz@gmail.com>
2014-12-25 01:18:42 +0900

    add tokyo

--- bot.rb
+++ bot.rb
@@ -12,10 +12,18 @@ Chatroid.new do |config|
   set :oauth_token_secret, ENV['ACCESS_SECRET']
 
   on_time :hour => 7, :min => 0, :sec => 0 do
+    #morioka
     forecast_json =  RestClient.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=030010'
     forecast = JSON.parse(forecast_json)
     forecast_morioka =  forecast["forecasts"][0]["telop"] + "で、最高気温は" + forecast["forecasts"][0]["temperature"]["max"]["celsius"] + "度"
     str = "今日の盛岡の天気は" + forecast_morioka + "です。"
+    tweet str
+
+    #tokyo
+    forecast_json =  RestClient.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
+    forecast = JSON.parse(forecast_json)
+    forecast_tokyo =  forecast["forecasts"][0]["telop"] + "で、最高気温は" + forecast["forecasts"][0]["temperature"]["max"]["celsius"] + "度"
+    str = "今日の東京の天気は" + forecast_tokyo + "です。"
     tweet str
   end
 end.run!

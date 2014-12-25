41643b05e435d549582bc55811171401908de1e4
otukutun <orehaorz@gmail.com>
2014-12-25 23:50:17 +0900

    fix

--- bot.rb
+++ bot.rb
@@ -3,27 +3,23 @@ require 'twitter'
 require 'rest_client'
 require 'json'
 
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_KEY']
+  config.access_token_secret = ENV['ACCESS_SECRET']
+end
 
-Chatroid.new do |config|
-  set :service, 'Twitter'
-  set :consumer_key, ENV['CONSUMER_KEY']
-  set :consumer_secret, ENV['CONSUMER_SECRET']
-  set :oauth_token, ENV['ACCESS_KEY']
-  set :oauth_token_secret, ENV['ACCESS_SECRET']
+#morioka
+forecast_json =  RestClient.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=030010'
+forecast = JSON.parse(forecast_json)
+forecast_morioka =  forecast["forecasts"][0]["telop"] + "で、最高気温は" + forecast["forecasts"][0]["temperature"]["max"]["celsius"] + "度"
+str = "今日の盛岡の天気は" + forecast_morioka + "です。"
+client.update str
 
-  on_time :hour => 23, :min => 40, :sec => 0 do
-    #morioka
-    forecast_json =  RestClient.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=030010'
-    forecast = JSON.parse(forecast_json)
-    forecast_morioka =  forecast["forecasts"][0]["telop"] + "で、最高気温は" + forecast["forecasts"][0]["temperature"]["max"]["celsius"] + "度"
-    str = "今日の盛岡の天気は" + forecast_morioka + "です。"
-    tweet str
-
-    #tokyo
-    forecast_json =  RestClient.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
-    forecast = JSON.parse(forecast_json)
-    forecast_tokyo =  forecast["forecasts"][0]["telop"] + "で、最高気温は" + forecast["forecasts"][0]["temperature"]["max"]["celsius"] + "度"
-    str = "今日の東京の天気は" + forecast_tokyo + "です。"
-    tweet str
-  end
-end.run!
+#tokyo
+forecast_json =  RestClient.get 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010'
+forecast = JSON.parse(forecast_json)
+forecast_tokyo =  forecast["forecasts"][0]["telop"] + "で、最高気温は" + forecast["forecasts"][0]["temperature"]["max"]["celsius"] + "度"
+str = "今日の東京の天気は" + forecast_tokyo + "です。"
+client.update str
